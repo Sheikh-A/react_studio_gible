27aacb5a8a4fc6665a76598aec3afc17cf1fc530
Ali Sheikh <ali.sheikh@flexport.com>
2020-08-23 14:06:22 -0700

    Updates

---
+++ precache-manifest.3302900e3a00d37356dd45d60543161e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc7a439976841ffee615946c40ca2e9",
+    "url": "/react_studio_gible/index.html"
+  },
+  {
+    "revision": "dcb1af0bee224a21a622",
+    "url": "/react_studio_gible/static/css/main.1eaf821e.chunk.css"
+  },
+  {
+    "revision": "bb9b351a3e835dafb973",
+    "url": "/react_studio_gible/static/js/2.aba82f17.chunk.js"
+  },
+  {
+    "revision": "dcb1af0bee224a21a622",
+    "url": "/react_studio_gible/static/js/main.7f8a7c5b.chunk.js"
+  },
+  {
+    "revision": "cd2725ced38dcb474890",
+    "url": "/react_studio_gible/static/js/runtime~main.af69e834.js"
+  },
+  {
+    "revision": "2d0769cbcdb8c0bbb0d11b802d8e0f9a",
+    "url": "/react_studio_gible/static/media/logo.2d0769cb.png"
+  }
+]);
